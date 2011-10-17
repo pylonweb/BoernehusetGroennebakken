30da65c4cc7fc06eb60d8f4556197d1d58055366
Johan Froelich <johan-frolich@hotmail.com>
2011-10-17 02:00:27 +0200

    faq design

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
 	$(".faq_answer").hide();
 
   $(".faq_question").click(function() {	
+  	if ($(this).children(".faq_answer").first().is(':hidden')) {
+  		$(this).children("a").first().children("span").first().html("&#9660;");
+  	}else{
+  		$(this).children("a").first().children("span").first().html("&#9654;");
+  	}
 
     $(this).children(".faq_answer").first().slideToggle();
+
+    
   });
 });
