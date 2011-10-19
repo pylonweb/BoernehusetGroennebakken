d4a3c1a82dd8214533d9402be1439794e2b9b036
Frederik Fenger <frederikfenger@gmail.com>
2011-10-19 20:41:30 +0200

    mysql2 adaptor

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_via, :remote_cache
 set :deploy_to, "/home/#{user}/#{domain}"
 set :use_sudo, false
 set :rvm_type, :user
- 
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
  
@@ -24,4 +24,44 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+end
+
+namespace :deploy do
+  namespace :db do
+    desc <<-DESC
+      Creates the database.yml configuration file in shared path.
+
+      By default, this task uses a template unless a template \
+      called database.yml.erb is found either is :template_dir \
+      or /config/deploy folders. The default template matches \
+      the template for config/database.yml file shipped with Rails.
+
+      When this recipe is loaded, db:setup is automatically configured \
+      to be invoked after deploy:setup. You can skip this task setting \
+      the variable :skip_db_setup to true. This is especially useful \ 
+      if you are using this recipe in combination with \
+      capistrano-ext/multistaging to avoid multiple db:setup calls \ 
+      when running deploy:setup for all stages one by one.
+    DESC
+    task :setup, :except => { :no_release => true } do
+
+      template = File.read("config/database.yml")
+      config = ERB.new(template)
+
+      run "mkdir -p #{shared_path}/db" 
+      run "mkdir -p #{shared_path}/config" 
+      put config.result(binding), "#{shared_path}/config/database.yml"
+    end
+
+    desc <<-DESC
+      [internal] Updates the symlink for database.yml file to the just deployed release.
+    DESC
+    task :symlink, :except => { :no_release => true } do
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
+    end
+
+  end
+
+  after "deploy:setup",           "deploy:db:setup"   unless fetch(:skip_db_setup, false)
+  after "deploy:finalize_update", "deploy:db:symlink"
 end
