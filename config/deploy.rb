b92360e97c33059b58d0637b4baa139bee6f9d90
Frederik Fenger <frederikfenger@gmail.com>
2011-10-19 19:22:51 +0200

    whatever

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :scm, "git"
 set :branch, "deploy"
 set :scm_verbose, true
 set :deploy_via, :remote_cache
-#set :scm_passphrase, "StarCraft"  # The deploy user's password
 set :deploy_to, "/home/#{user}/#{domain}"
 set :use_sudo, false
 set :rvm_type, :user
