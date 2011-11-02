f17ec9bf78e0f4d73905f9e2ffc58f1e73111101
Frederik Fenger <frederikfenger@gmail.com>
2011-11-02 01:35:22 +0100

    /faqs skal give 404 error

--- vendor/engines/faqs/app/controllers/faqs_controller.rb
+++ vendor/engines/faqs/app/controllers/faqs_controller.rb
@@ -1,22 +1,8 @@
 class FaqsController < ApplicationController
 
-  before_filter :find_all_faqs
-  before_filter :find_page
-
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
     # by swapping @page for @faq in the line below:
     error_404
   end
-
-protected
-
-  def find_all_faqs
-    @faqs = Faq.order('position ASC')
-  end
-
-  def find_page
-    @page = Page.where(:link_url => "/faqs").first
-  end
-
 end
