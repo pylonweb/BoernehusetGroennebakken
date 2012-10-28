7c7ec08f9d4c1c0ed139174b3631a3a5f3c69f53
Frederik Fenger <frederikfenger@gmail.com>
2012-10-28 15:11:07 +0800

    newrelic bug

--- config/recipes/newrelic.rb
+++ config/recipes/newrelic.rb
@@ -4,4 +4,9 @@ namespace :newrelic do
 		template "newrelic.yml.erb", "#{shared_path}/config/newrelic.yml"
 	end
 	after "deploy:setup", "newrelic:setup"
+
+	task :symlink, roles: :app do
+    run "ln -nfs #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
+	end
+	after "deploy:finalize_update", "newrelic:symlink"
 end
